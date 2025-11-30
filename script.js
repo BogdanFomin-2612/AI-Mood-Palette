4e77c0a189fb1ac98562e1af078e1929ca6fd3f6
Bogdan Fomin <boduakontact@gmail.com>
2025-11-30 17:16:31 +0100

    css and js changes

---
+++ script.js
@@ -0,0 +1,5 @@
+const promptInput = document.getElementById('prompt');
+const generateBtn = document.getElementById('generate-btn');
+generateBtn.addEventListener('click', () => {
+    console.log(promptInput.value);
+});
